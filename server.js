745738a58c3c5e770ce8a31394511d5cb8a83011
Zaheer Abbas <zaheerabbas@192.168.10.16>
2022-11-30 00:43:57 +0500

    script updated

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post("", async (req, res) => {
 	};
 })
 
-app.listen(3000, function () {
-	console.log("Server started on port 3000");
-});
+// app.listen(3000, function () {
+// 	console.log("Server started on port 3000");
+// });
 
