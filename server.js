8fac1a75f48ba696b8c824823c77ec0d1dfe8719
Zaheer Abbas <zaheerabbas@192.168.10.11>
2022-12-17 00:33:21 +0500

    msg sync api added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { decrypt, getSignature } from '@wecom/crypto';
 import bodyParser from "body-parser";
 import xmlparser from 'express-xml-bodyparser';
-import xmlParser from 'xml2json';
+import axios from 'axios';
 
 const app = express();
 
@@ -17,16 +17,6 @@ app.get("/", async (req, res) => {
 	console.log(req.query);
 	const { msg_signature, timestamp, nonce, echostr } = req.query;
 	let weToken = 'fdAhCoBdeB1sRdDh4sqorXD7';
-	// try {
-	// 	const apiResponse = await fetch(
-	// 		'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wweb89d875de036888&corpsecret=vnVJ01v1uvdPvzoxhi1Ya82KDC4xut6jH9xn_LmU848'
-	// 	);
-	// 	const apiResponseJson = await apiResponse.json()
-	// 	weToken = apiResponseJson.access_token;
-	// } catch (err) {
-	// 	console.log(err)
-	// 	res.status(500).send('Something went wrong')
-	// }
 
 	const devSign = getSignature(weToken, timestamp, nonce, echostr);
 	console.log("Dev Singature ", devSign);
@@ -53,13 +43,28 @@ app.post("/", async (req, res) => {
 	// console.log('Parsed XML: ' + JSON.stringify(req.body));
 	// console.log('JSON output', xmlParser.toJson(`${req.body}`));
 	if (isAuthorized(req)) {
-		const encodingAESKey = 'ecFa4cslc2lNetLEUjbH7DcPi8PV9JfaB1xu1IELBTR';
-		const echostr = req.body.xml.encrypt[0];
-		const message = decrypt(encodingAESKey, echostr);
+		// const encodingAESKey = 'ecFa4cslc2lNetLEUjbH7DcPi8PV9JfaB1xu1IELBTR';
+		// const echostr = req.body.xml.encrypt[0];
+		// const message = decrypt(encodingAESKey, echostr);
 
-		console.log(message);
-		
-		res.status(200).send(message);
+		// console.log(message);
+		let accessToken = cache.get("access_token");
+		if (!accessToken) {
+			// if the access token is not in the cache, fetch a new one
+			accessToken = await fetchAccessToken();
+			cache.set("access_token", accessToken);
+		}
+
+		try {
+			const response = await axios.post(`https://qyapi.weixin.qq.com/cgi-bin/kf/sync_msg?access_token=${accessToken}`, {
+				"open_kfid": "wkA816UQAANyEz3O8fUwfiGQelx4VF_w"
+			});
+			console.log("Result", response.data);
+			res.status(200);
+		} catch (err) {
+			console.log("Error", err);
+			res.status(400);
+		}
 	} else {
 		res.status(401).send('Unauthorised User')
 	};
@@ -69,13 +74,26 @@ const isAuthorized = (req) => {
 	const { msg_signature, timestamp, nonce } = req.query;
 	const echostr = req.body.xml.encrypt[0];
 	let weToken = 'fdAhCoBdeB1sRdDh4sqorXD7';
-	
+
 	const devSign = getSignature(weToken, timestamp, nonce, echostr);
 
 	if (devSign === msg_signature) {
 		return true;
 	} else {
 		return false
+	}
+}
+
+const fetchAccessToken = async () => {
+	try {
+		const apiResponse = await fetch(
+			'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wweb89d875de036888&corpsecret=vnVJ01v1uvdPvzoxhi1Ya82KDC4xut6jH9xn_LmU848'
+		);
+		const apiResponseJson = await apiResponse.json()
+		return apiResponseJson.access_token;
+	} catch (err) {
+		console.log(err)
+		res.status(500).send('Something went wrong')
 	}
 }
 
